47e0e5ffede5d1fdf13ca612bac85c1186390b8d
bacngv <bac17012004@gmail.com>
2024-11-15 20:40:54 +0700

    update

--- pdf_processing.py
+++ pdf_processing.py
@@ -6,23 +6,48 @@ from visualization import visualize_detected_tables, visualize_cropped_table
 from ocr import apply_ocr, apply_ocr_remaining_area
 from io_utils import save_remaining_text_to_txt, save_to_csv
 from torchvision import transforms
-import torch 
+import torch
+import cv2
+import numpy as np
+import RRDBNet_arch as arch 
 
-# Main function to process the PDF file
+def load_super_resolution_model(model_path='./PDF2TEXT-UDP/models/RRDB_ESRGAN_x4.pth', device=torch.device('cuda')):
+    model = arch.RRDBNet(3, 3, 64, 23, gc=32)
+    model.load_state_dict(torch.load(model_path), strict=True)
+    model.eval()
+    model = model.to(device)
+    return model
+
+def apply_super_resolution(image, model, device):
+    img = np.array(image)[:, :, ::-1]  
+    img = img * 1.0 / 255  
+    img = torch.from_numpy(np.transpose(img, (2, 0, 1))).float().unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        output = model(img).squeeze().float().cpu().clamp_(0, 1).numpy()
+    output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))  
+    output = (output * 255.0).round().astype(np.uint8)  
+    return output
+
 def process_pdf(pdf_path, output_folder):
-    # Convert PDF to images
     images = convert_from_path(pdf_path)
-    
+
+
     model, device = load_detection_model()
     structure_model = load_structure_model(device)
-    
+    sr_model = load_super_resolution_model(device=device)  
+
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     for page_num, image in enumerate(images):
         print(f"Processing page {page_num + 1}/{len(images)}")
 
-        # Detect tables in the image
+        sr_image = apply_super_resolution(image, sr_model, device)
+        sr_image = cv2.cvtColor(sr_image, cv2.COLOR_BGR2RGB)  
+        image = transforms.ToPILImage()(torch.tensor(sr_image).permute(2, 0, 1))  
+
+        
         pixel_values = preprocess_image(image)
         outputs = detect_objects(model, pixel_values, device)
 
@@ -30,20 +55,15 @@ def process_pdf(pdf_path, output_folder):
         id2label[len(model.config.id2label)] = "no object"
         objects = outputs_to_objects(outputs, image.size, id2label)
 
-        # Visualize detected tables
         visualize_detected_tables(image, objects)
 
-        # Create a mask for the detected table bounding boxes
         detected_bboxes = [obj['bbox'] for obj in objects if obj['label'] in ['table', 'table rotated']]
 
-        # Create a new image with the table areas removed
         image_without_tables = remove_table_areas(image, detected_bboxes)
 
-        # Perform OCR on the remaining area of the image
         remaining_text = apply_ocr_remaining_area(image_without_tables)
         save_remaining_text_to_txt(remaining_text, output_folder, page_num)
 
-        # Process each cropped table and save data as CSV
         table_data_list = []
         for idx, bbox in enumerate(detected_bboxes):
             x_min, y_min, x_max, y_max = bbox
@@ -51,7 +71,6 @@ def process_pdf(pdf_path, output_folder):
             table_data = process_cropped_table(cropped_table, structure_model, device, page_num, idx, output_folder)
             table_data_list.append(table_data)
 
-# Function to process each cropped table and save to CSV
 def process_cropped_table(cropped_table, structure_model, device, page_num, table_index, output_folder):
     structure_transform = transforms.Compose([
         MaxResize(1000),
@@ -62,46 +81,18 @@ def process_cropped_table(cropped_table, structure_model, device, page_num, tabl
     pixel_values = structure_transform(cropped_table).unsqueeze(0)
     pixel_values = pixel_values.to(device)
 
-    # Forward pass
     with torch.no_grad():
         outputs = structure_model(pixel_values)
-
-    # Update id2label to include "no object"
     structure_id2label = structure_model.config.id2label
     structure_id2label[len(structure_id2label)] = "no object"
 
     cells = outputs_to_objects(outputs, cropped_table.size, structure_id2label)
-
-    # Visualize the detected cells in the cropped table
     visualize_cropped_table(cropped_table, cells)
 
-    # Apply OCR to the detected cells
     cell_coordinates = get_cell_coordinates_by_row(cells)
     data = apply_ocr(cell_coordinates, cropped_table)
 
-    # Save extracted data to CSV
     csv_filename = os.path.join(output_folder, f'page_{page_num + 1}_table_{table_index + 1}.csv')
     save_to_csv(data, csv_filename)
 
     return data
-
-def get_cell_coordinates_by_row(table_data):
-    rows = [entry for entry in table_data if entry['label'] == 'table row']
-    columns = [entry for entry in table_data if entry['label'] == 'table column']
-
-    rows.sort(key=lambda x: x['bbox'][1])
-    columns.sort(key=lambda x: x['bbox'][0])
-
-    def find_cell_coordinates(row, column):
-        cell_bbox = [column['bbox'][0], row['bbox'][1], column['bbox'][2], row['bbox'][3]]
-        return cell_bbox
-
-    cell_coordinates = []
-    for row in rows:
-        row_cells = []
-        for column in columns:
-            cell_bbox = find_cell_coordinates(row, column)
-            row_cells.append({'bbox': cell_bbox})
-        cell_coordinates.append(row_cells)
-
-    return cell_coordinates
